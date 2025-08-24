2b7c1730bae55f9b07c5757199c78dd3bd927832
Tristan Rivoallan <tristan@rivoallan.net>
2025-08-24 02:01:39 +0200

    feat: ajouter la configuration Terraform pour le volume et l'attachement sur Hetzner (#5)
    * feat: ajouter la configuration Terraform pour le volume et l'attachement sur Hetzner
    
    * feat: ajouter la configuration du provider Hetzner et la variable d'API
    
    * fix: corriger l'attachement du volume en utilisant le serveur "cartons"
    
    * fix: corriger la référence au serveur dans l'attachement du volume
    
    * fix: datacenter
    
    * deffe

---
+++ infrastructure/terraform/hetzner.tf
@@ -0,0 +1,27 @@
+variable "hcloud_token" {
+  description = "Hetzner Cloud API token"
+  type        = string
+  sensitive   = true
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+data "hcloud_server" "cartons" {
+  name = "cartons"
+}
+
+resource "hcloud_volume" "superlarsen" {
+  name      = "superlarsen"
+  size      = 10 
+  location  = "hel1-dc2"
+  format    = "ext4"
+  automount = true
+  delete_protection = true
+}
+
+resource "hcloud_volume_attachment" "superlarsen_attachment" {
+  volume_id = hcloud_volume.superlarsen.id
+  server_id = data.hcloud_server.cartons.id
+}
